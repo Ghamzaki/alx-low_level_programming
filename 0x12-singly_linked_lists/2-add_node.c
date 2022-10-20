db25804428abbb933e7fa7959a34cc68e45a95ea
Ghamzaki <111054235+Ghamzaki@users.noreply.github.com>
2022-10-20 17:57:45 +0100

    string linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,27 @@
 #include "lists.h"
-#include <string.h>
-
 /**
- * *add_node - adds a node to a linked list head
- * @head: linked list head
- * @str: string element of the linked list
- * Return: number of elements
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	int len_str;
+	list_t *new;
+	size_t a = 0;
 
-	new_node = (list_t *) malloc(sizeof(list_t));
-
-	if (new_node == NULL)
-	{
-		free(new_node);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	}
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	for (len_str = 0; str[len_str] != '\0';  len_str++)
-	;
+	while (str[a])
+		a++;
 
-	new_node->len = len_str;
-	new_node->next = *head;
-	*head = new_node;
+	new->str = strdup(str);
+	new->len = a;
+	new->next = *head;
+	*head = new;
 
-	return (new_node);
+	return (*head);
 }
