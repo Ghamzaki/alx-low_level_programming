1a5e8f557f3dc9cba3a0539e9c05dbce7dca3477
Ghamzaki <111054235+Ghamzaki@users.noreply.github.com>
2022-10-31 20:36:38 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,30 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * create_file - create a file with read/write access for user
- * @filename: name of file to create
- * @text_content: string to write to file
- * Return: 1 on success, -1 on failure
+ * create_file - creates a file and adds permission to it
+ * @filename: The name of the file to be created
+ * @text_content: content to be placed in the created file
+ * Return: 1 if successfull and -1 on err.
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, rstatus, i;
+	ssize_t fild = 0, chk = 0, i = 0;
 
-if (filename == NULL)
-	return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-if (fd == -1)
-	return (-1);
-
-if (text_content)
-{
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	rstatus = write(fd, text_content, i);
-	if (rstatus == -1)
+	if (!filename)
 		return (-1);
-}
 
-close(fd);
-return (1);
+	if (!text_content)
+		text_content = "";
+
+	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	if (fild < 0)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+	chk = write(fild, text_content, i);
+	if (chk < 0)
+		return (-1);
+
+	close(fild);
+	return (1);
 }
