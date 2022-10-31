1d3167255afb3fd42db3a11ef8da641e4f6c7f9d
Ghamzaki <111054235+Ghamzaki@users.noreply.github.com>
2022-10-31 20:35:44 +0100

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,32 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to the file name
- * @letters: the number of letters it should read and print
- * Return:  the actual number of letters it could read and print
-*/
-
+ * read_textfile - Reads a textfile and prints the contents to the POSIX STDOUT
+ * @filename: The name of the file to read from
+ * @letters: The number of characters it should print to the STDOUT
+ * Return: Returns the number of characters printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t fild = 0, chk = 0;
+	char *buffer;
 
-int file;
-ssize_t rcount, wcount;
-char *buffer;
+	if (!filename || !letters)
+		return (0);
 
-if (filename == NULL)
-	return (0);
+	fild = open(filename, O_RDONLY);
+	if (fild < 0)
+		return (0);
 
-file = open(filename, O_RDWR);
-if (file == -1)
-	return (0);
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
+	chk = read(fild, buffer, letters);
+	chk = write(STDOUT_FILENO, buffer, chk);
+	if (chk < 0)
+		return (0);
+
+	close(fild);
 	free(buffer);
-	return (0);
-}
-
-rcount = read(file, buffer, letters);
-if (rcount == -1)
-	return (0);
-
-wcount = write(STDOUT_FILENO, buffer, rcount);
-
-if (wcount == -1 || rcount != wcount)
-	return (0);
-
-free(buffer);
-
-close(file);
-
-return (wcount);
+	return (chk);
 }
