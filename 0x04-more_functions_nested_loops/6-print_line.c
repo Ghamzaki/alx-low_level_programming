f46cd5ff719472cbe6e618901f9fb890b17d0757
Ghamzaki <uthmanghamzaki@gmail.com>
2023-08-17 19:24:22 +0000

    Numbers constitute the only universal languag

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: number of character
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar('_');
+	n++;
+	}
+	_putchar('\n');
+}
