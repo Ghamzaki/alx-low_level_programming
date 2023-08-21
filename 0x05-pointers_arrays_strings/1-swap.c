65038999dde96c9e84469f3ea8fcb5d26e19b9e3
Ghamzaki <uthmanghamzaki@gmail.com>
2023-08-21 07:42:40 +0000

    98 Betty st

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - waps the values of two integers
+ * @a: value to swap
+ * @b: value to swap
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{:wq
+
+	*a = &b;
+	*b = &a;
+}
