fd8e4c033a9dcd8c3481a2cc3691c8635b210551
Ghamzaki <uthmanghamzaki@gmail.com>
2023-08-21 07:10:29 +0000

    98 Battery st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#definr MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 int _atoi(char *s);
@@ -13,5 +13,6 @@ void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+int _putchar(char c);
 
 #endif
