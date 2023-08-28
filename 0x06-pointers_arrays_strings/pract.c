fd005c32d34dc413bbb4ec39bdb7402cda4fc213
Ghamzaki <uthmanghamzaki@gmail.com>
2023-08-28 20:57:02 +0000

    practice

---
+++ 0x06-pointers_arrays_strings/pract.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	
+}
