f0006cd9f4ef92aa2871d42148b6702e488f29b5
Ghamzaki <uthmanghamzaki@gmail.com>
2023-09-27 22:35:10 +0000

    free mem.

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list.
+ * @head: memory to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
